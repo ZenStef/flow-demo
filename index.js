a6ee34ac8fb78b90151f7957a19f032870c3c344
ZenStef <svarga@zendesk.com>
2019-07-03 20:39:03 -0700

    reverting

--- index.js
+++ index.js
@@ -68,19 +68,8 @@ app.get('/follow', function(request, response) {
   response.render('follow.html', { env: envName});
 });
 
-app.use(app.router);
-
-app.use(function(req, res, next){
-  res.status(404);
-  if (req.accepts('html')) {
-    res.render('404', { url: req.url });
-    return;
-  }
-  if (req.accepts('json')) {
-    res.send({ error: 'Not found' });
-    return;
-  }
-  res.type('txt').send('Not found');
+app.get('*', function(request, response) {
+  response.status(404).send('what???');
 });
 
 app.listen(app.get('port'), function() {
